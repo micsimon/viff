6abab92290ce5d80287dab7be76d3f8a9cfb4d20
Michael <mail@michaelsimon.io>
2017-09-19 19:53:04 +0200

    add application module with base spring boot app

---
+++ viff-application/src/main/java/micsimon/viff/ViffApplication.java
@@ -0,0 +1,12 @@
+package micsimon.viff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViffApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ViffApplication.class, args);
+    }
+}
---
+++ viff-application/src/test/java/micsimon/viff/ViffApplicationTests.java
@@ -0,0 +1,16 @@
+package micsimon.viff;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ViffApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
